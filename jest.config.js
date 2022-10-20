ce71a12cc84116d2ce589168991cede9fb22ffac
Yash <yash-vardhan@hotmail.com>
2022-10-20 14:25:52 +0530

    removed jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-  preset: 'react-native',
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: ['node_modules/?!(react-icons)'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-};
